d440bdfea002f776908415a9fbfad520d4e83bf1
c0v1d-9119361 <74178900+c0v1d-9119361@users.noreply.github.com>
2023-06-02 16:04:54 +0800

    v0.6.20
    UnBoosted

--- js/technical/systemComponents.js
+++ js/technical/systemComponents.js
@@ -146,7 +146,7 @@ var systemComponents = {
 		<br><br>
         Time Played: {{ formatTime(player.timePlayed) }}<br><br>
         <h3>Hotkeys</h3><br>
-        <span v-for="key in hotkeys" v-if="player[key.layer].unlocked && tmp[key.layer].hotkeys[key.id].unlocked"><br>{{key.description}}</span></div>
+        <span v-for="key in hotkeys" v-if="player[key.layer].unlocked && tmp[key.layer].hotkeys[key.id].unlocked"><br>{{tmp[key.layer].hotkeys[key.id].description}}</span></div>
     `
     },
 
